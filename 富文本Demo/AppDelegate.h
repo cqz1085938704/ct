558eaacb9a611bad2eef3a7f5d9600c3f0b4e09b
Henry's Mac <1085938704@qq.com>
2015-11-09 10:34:46 +0800

    Initial Commit

---
+++ 富文本Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  富文本Demo
+//
+//  Created by caiyao's Mac on 15/11/9.
+//  Copyright © 2015年 core's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
